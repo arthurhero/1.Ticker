83a21bdbe43efc581cf88ff53d65e5b761d3fe77
Ziwen Chen <arthurhero@Ziwens-MacBook.local>
2016-11-06 11:18:15 -0600

    Initial Commit

---
+++ Ticker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ticker
+//
+//  Created by Ziwen Chen on 11/6/16.
+//  Copyright © 2016 Ziwen Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
