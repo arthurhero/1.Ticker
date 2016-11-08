3062f5f6edea45ff02f2eff7c68cb09d3f8fdde9
Ziwen Chen <arthurhero@Ziwens-MacBook.local>
2016-11-07 20:38:44 -0600

    second commit

--- Ticker/ViewController.m
+++ Ticker/ViewController.m
@@ -9,10 +9,27 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *animalField;
+@property (weak, nonatomic) IBOutlet UITextField *verbField;
+
+@property (weak, nonatomic) IBOutlet UILabel *outputLabel;
+
 
 @end
 
 @implementation ViewController
+
+- (IBAction)didTapButton:(id)sender {
+        _outputLabel.text = [self name:_nameField.text animal:_animalField.text verb:_verbField.text];
+}
+
+
+- (NSString *) name:(NSString *)nm animal: (NSString *)ani verb: (NSString *) ver{
+    return [NSString stringWithFormat:@"You %@ %@, but %@ does not.",ver,ani,nm];
+
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
